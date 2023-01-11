1f4ffc267243704a4d464df0ba1615a68d08b551
Joe <joeaoregan@gmail.com>
2023-01-11 15:50:58 +0000

    fix names, team member email

--- daie_ca4_data.sql
+++ daie_ca4_data.sql
@@ -1,5 +1,5 @@
 -- Created by Vertabelo (http://vertabelo.com)
--- Last modification date: 2023-01-11 14:13:59.238
+-- Last modification date: 2023-01-11 15:34:12.036
 
 -- tables
 -- Table: Asset
@@ -8,20 +8,20 @@ CREATE TABLE Asset (
     Name varchar(50) NOT NULL,
     Type varchar(50),
     Format varchar(50),
-    Created_by integer NOT NULL,
+    Created_By integer NOT NULL,
     Date_Created date NOT NULL,
-    CONSTRAINT Asset_Team_Member FOREIGN KEY (Created_by)
+    CONSTRAINT Asset_Team_Member FOREIGN KEY (Created_By)
     REFERENCES Team_Member (Id)
 );
 
 -- Table: Asset_Work_Items
 CREATE TABLE Asset_Work_Items (
     Work_Item_Id integer NOT NULL,
-    Asset_id integer NOT NULL,
-    CONSTRAINT Asset_Work_Items_pk PRIMARY KEY (Work_Item_Id,Asset_id),
+    Asset_Id integer NOT NULL,
+    CONSTRAINT Asset_Work_Items_pk PRIMARY KEY (Work_Item_Id,Asset_Id),
     CONSTRAINT Asset_Work_Item_Work_Item FOREIGN KEY (Work_Item_Id)
     REFERENCES Work_Item (Id),
-    CONSTRAINT Asset_Work_Items_Asset FOREIGN KEY (Asset_id)
+    CONSTRAINT Asset_Work_Items_Asset FOREIGN KEY (Asset_Id)
     REFERENCES Asset (Id)
 );
 
@@ -52,11 +52,11 @@ CREATE TABLE Project (
 -- Table: Project_Team
 CREATE TABLE Project_Team (
     Team_Member_Id integer NOT NULL,
-    Project_Project_Id integer NOT NULL,
-    CONSTRAINT Project_Team_Id PRIMARY KEY (Project_Project_Id,Team_Member_Id),
+    Project_Id integer NOT NULL,
+    CONSTRAINT Project_Team_Id PRIMARY KEY (Project_Id,Team_Member_Id),
     CONSTRAINT Team_Team_Member FOREIGN KEY (Team_Member_Id)
     REFERENCES Team_Member (Id),
-    CONSTRAINT Team_Project FOREIGN KEY (Project_Project_Id)
+    CONSTRAINT Team_Project FOREIGN KEY (Project_Id)
     REFERENCES Project (Id)
 );
 
@@ -77,8 +77,10 @@ CREATE TABLE Team_Member (
     Id integer NOT NULL CONSTRAINT Team_Member_pk PRIMARY KEY AUTOINCREMENT,
     First_Name varchar(50) NOT NULL,
     Last_Name varchar(50) NOT NULL,
-    Role_id integer NOT NULL,
-    CONSTRAINT Team_Member_Role FOREIGN KEY (Role_id)
+    Email varchar(128) NOT NULL,
+    Role_Id integer NOT NULL,
+    CONSTRAINT Team_Member_Email_AK UNIQUE (Email),
+    CONSTRAINT Team_Member_Role FOREIGN KEY (Role_Id)
     REFERENCES Role (Id)
 );
 
@@ -87,10 +89,10 @@ CREATE TABLE Work_Item (
     Id integer NOT NULL CONSTRAINT Work_Item_pk PRIMARY KEY AUTOINCREMENT,
     Name varchar(50) NOT NULL,
     Status_Id integer NOT NULL,
-    Assigned_to integer NOT NULL,
+    Assigned_To integer,
     CONSTRAINT Work_Item_Statuses FOREIGN KEY (Status_Id)
     REFERENCES Status (Id),
-    CONSTRAINT Work_Item_Team_Member FOREIGN KEY (Assigned_to)
+    CONSTRAINT Work_Item_Team_Member FOREIGN KEY (Assigned_To)
     REFERENCES Team_Member (Id)
 );
 
