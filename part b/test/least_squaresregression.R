63b887627cbcde3087376788134d5f799a166671
Joe <joeaoregan@gmail.com>
2023-01-19 05:54:51 +0000

    tidy

---
+++ part b/test/least_squaresregression.R
@@ -0,0 +1,44 @@
+# Linear Least Squares Regression
+# https://www.cyclismo.org/tutorial/R/linearLeastSquares.html
+
+cat("\014") # clear screen
+par(mfrow=c(1,1)) # reset plotting region
+
+year <- c(2000 ,   2001  ,  2002  ,  2003 ,   2004)
+rate <- c(9.34 ,   8.50  ,  7.62  ,  6.93  ,  6.60)
+
+plot(year,rate,
+     main="Commercial Banks Interest Rate for 4 Year Car Loan",
+     sub="http://www.federalreserve.gov/releases/g19/20050805/")
+cor(year,rate)
+
+fit <- lm(rate ~ year)
+fit
+
+attributes(fit)
+
+fit$coefficients[1]
+fit$coefficients[[1]]
+fit$coefficients[2]
+fit$coefficients[[2]] # just the number
+
+fit$coefficients[[2]]*2015+fit$coefficients[[1]]
+
+res <- rate - (fit$coefficients[[2]]*year+fit$coefficients[[1]])
+res
+plot(year,res)
+
+residuals(fit)
+fit$residuals
+plot(year,fit$residuals)
+
+plot(year,rate,
+     main="Commercial Banks Interest Rate for 4 Year Car Loan",
+     sub="http://www.federalreserve.gov/releases/g19/20050805/")
+
+abline(fit)
+
+
+
+summary(fit)
+
